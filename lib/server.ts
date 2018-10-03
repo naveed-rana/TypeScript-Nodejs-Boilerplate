b210766de609551ba0b523fd87bfde20fb68c4c6
Naveed-rana <rana.naveed812@gmail.com>
2018-10-03 16:23:06 +0500

    remove jest config

--- lib/server.ts
+++ lib/server.ts
@@ -4,11 +4,11 @@ import errorhandler  from "errorhandler";
 //get env PORT from server or use 8080
 const PORT = process.env.PORT || 8080;
 
+
 //error handler midelware
-if (process.env.NODE_ENV === 'development') {
-  // only use in development
+// only use in development
   App.use(errorhandler());
-}
+
 
 
 App.listen(PORT, () => {
